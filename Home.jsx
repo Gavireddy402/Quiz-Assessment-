f1a0d1d118cc1a0124ea90dfaea1259e3c52deb8
Gavireddy402 <141010021+Gavireddy402@users.noreply.github.com>
2024-03-26 22:33:36 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+       <img src="https://cdn.slidesharecdn.com/ss_thumbnails/quiz-150613141251-lva1-app6891-thumbnail-4.jpg?cb=1434204857" width={500} height={600}/>
+        <h1>Lets Enhance Your SkIlls</h1>
+      </div>
+    )
+  }
